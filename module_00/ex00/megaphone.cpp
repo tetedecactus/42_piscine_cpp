4c02c8cfd98d557b147355160be2d373c90bfa28
bob <o.labrecque.lacasse@gmail.com>
2022-03-14 10:00:58 -0400

    ex00 fini fini

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 17:45:37 by olabrecq          #+#    #+#             */
-/*   Updated: 2021/12/20 21:54:21 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/03/11 09:42:46 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 
 int main(int ac, char **av)
 {
-    if (ac == 1)
-        std::cout << "* LOUD AND UMBEARABLE FEEDBACK NOISE *";
-    else
-    {
-        for (int i = 1; i < ac; i++)
-        {
-            int j = 0;
-            while(av[i][j])
-                std::cout << char(toupper(av[i][j++]));
-        }
-    }   
+    if (ac == 1) {
+        std::cout << "* LOUD AND UMBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < ac; i++) 
+    for (int j = 0; av[i][j]; j++)  
+            std::cout << char(toupper(av[i][j]));
     std::cout << std::endl;
     return (0); 
 }
