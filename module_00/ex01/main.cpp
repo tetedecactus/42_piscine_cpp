3cf035ae9b402efa006e33424ee25a1a43e21660
bob <o.labrecque.lacasse@gmail.com>
2022-04-24 18:04:46 -0400

    note++

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 21:12:31 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/04/24 18:01:34 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/04/24 18:04:01 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int main ()
 {
 	phonebook input;
 	std::cout << "\t\t📞--->PHONEBOOK<---📞\n" << std::endl;
+	std::cout << "*******************************************************\n" << std::endl;
 	std::cout << "PLEASE ENTER COMMAND\n" << std::endl;
 	std::cout << "ADD" << std::endl;
 	std::cout << "SEARCH" << std::endl;
