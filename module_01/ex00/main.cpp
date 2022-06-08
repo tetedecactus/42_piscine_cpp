f2b3467634704a5ee21ae96dcaac4106a23bdad6
bob <o.labrecque.lacasse@gmail.com>
2022-06-08 08:12:34 -0400

    finir m01 ex00

--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:28:50 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/06/07 20:55:02 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/06/08 08:10:30 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ int main() {
 	
 	std::cout << GREEN << "Ce qu'on va faire, on va creer 2 Zombies, avec 2 fonctions differente\n" << RESET << std::endl; sleep(3);
 	std::cout << GREEN << "Premiere fonction -> :" << PINK << " Zombie* newZombie(std::string name)" << RESET << std::endl;
+	std::cout << RED << "IMPORTANT, cette fonction cree un Zombie sur la HEAP, il est declarer comme suis : " << YELLOW << "Zombie* new " << RED <<" newZombie\n\n" << RESET << std::endl;
 	std::cout << GREEN << "Deuxieme fonction -> :" << PINK << " randomChump(std::string name)" << RESET << std::endl; sleep(2);
+	std::cout << RED << "IMPORTANT, cette fonction cree un Zombie sur la STACK, il est declarer comme suis : " << YELLOW << "Zombie" << RED << " newZombie\n" << RESET << std::endl; 
+	
 	std::cout << BLUE << "Entre le nom du premier zombie " << std::endl;
 	std:getline(std::cin, firstNameInput); firstNameInput = "Pere-Noel"; sleep(2);
 	std::cout << "Premier Zombie : " << PINK << firstNameInput << RESET << std::endl; sleep(2);
@@ -40,9 +43,10 @@ int main() {
 	std::cout << GREEN << "Check on va commencer.. sans commantaire..." << RESET << std::endl; sleep(2);
 	std::cout << RED << "IMPORTANT!!" << RESET << std::endl; sleep(2);
 	
-	std::cout << GREEN << "On cree le premier Zombie en apellant le constructeur -> : " << PINK << "Zombie fisrtZombie(\"Pere-Noel\")" << RESET << std::endl;
+	std::cout << GREEN << "On cree le premier Zombie en apellant le constructeur -> : " << PINK << "Zombie fisrtZombie(\"Pere-Noel\")" << RESET << std::endl; sleep(3);
+	Zombie firstZombie("Pere-Noel");
 	std::cout << BLUE << "Bon fack la On va envoyer Pere-Noel a :" << RED << " Zombie* newZombie(\"Pere-Noel\")" << RESET << std::endl;
-	Zombie fisrtZombie("Pere-Noel");
+	firstZombie.newZombie("Pere-Noel");
 	std::cout << BLUE << "Maintenant que Pere-Noel est vivant on l'envois dans la fonction " << PINK << " Zombie* newZombie(std::string name)" << RESET << std::endl;
 	
 	
