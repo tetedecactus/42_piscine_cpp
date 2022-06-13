5fe8f4bf96273a3a7a92de4e4db2fa77a1fb403a
Olivier Labrecque-lacasse <olabrecq@c2r6p10.42quebec.com>
2022-06-13 14:58:15 -0400

    capo-light

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,13 +6,21 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:28:50 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/06/08 11:13:10 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/06/13 14:41:57 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
+#include <string>
+#include <sstream>
+
+# define BLUE "\033[0;36m"
+# define PINK "\033[0;35m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define RED "\033[0;31m"
+# define RESET "\033[0m"
 
 //Create a program that takes three parameters in the following order: a filename and
 //two strings, s1 and s2.
@@ -20,28 +28,33 @@
 //It will open the file <filename> and copies its content into a new file
 //<filename>.replace, replacing every occurrence of s1 with s2.
 
+//
 int main(int ac, char** av) {
 
-    int i;
-    std::string s1;
-    std::string s2;
-    std::ifstream ifs;
-//    (void)av;
-    i = 0;
-    if (ac == 4) {
-        ifs.open(av[2]);
-//        s1 = av[3];
-//        s2 = av[4];
-//        std::cout << "s1 : " << s1 <<std::endl;
-//        std::cout << "s2 : " << s2 <<std::endl;
-//
-////        ifs >> s1 >> s2;
-//        std::cout << "s1 : " << s2 <<std::endl;
-//        ifs.close();
-        printf("inside\n");
-        //Use a ofstream qui joint le ac[1] et ."replace"
-    }
-    else {
-        std::cout << "Example of valid input: ./{PROG} {FileName} {String1} {String2}" << std::endl;
-    }
+    std::ifstream myFile;
+	printf(" ac = %d", ac);	
+    if (ac != 4) {
+        std::cout << RED << "Example of valid input: ./{PROG} {FileName} {String1} {String2}" << std::endl; 
+		return 1;
+	}
+	std::string fileName(av[1]);
+	std::cout << fileName << std::endl;
+	
+	if (fileName == "file.txt") {
+		std::cout << "INSIDE" << std::endl;
+		myFile.open(fileName, std::ios::in);
+		if (myFile.is_open()) {
+			std::string s1, s2, line; // Creating variable i need
+			while (std::getline(myFile, line)) {
+				std::cout << line << std::endl;
+				// Faire de string compare
+			}
+			myFile.close();
+		}
+	}
+	else {
+		std::cout << RED << "Your second argument need to be \"file.txt\"" << RESET << std::endl; 
+		return 1;
+	}
+    
 }
