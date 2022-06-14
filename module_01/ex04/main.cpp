9fccafe0f46c16aa3bb0372413bed9c6066ebbe1
Olivier Labrecque-lacasse <olabrecq@c2r6p10.42quebec.com>
2022-06-14 12:42:55 -0400

    m01 ex04 fini

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:28:50 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/06/13 20:13:08 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/06/14 12:37:43 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,39 +35,54 @@
 int main(int ac, char** av) 
 {
     std::ifstream myFile;
-	printf(" ac = %d\n", ac);	
     if (ac != 4) 
 	{
         std::cout << RED << "Example of valid input: ./{PROG} {FileName} {String1} {String2}" << std::endl; 
 		return 1;
 	}
-	std::string fileName(av[1]);
-	std::cout << fileName << std::endl;
-	
-	if (fileName == "file.txt") 
+	std::string fileName(av[1]);// Cree une string a partir du av[1]
+	std::string replaceFileName(av[1]);
+	std::size_t foundLast = replaceFileName.rfind(replaceFileName);
+	if (foundLast == std::string::npos)
 	{
-		myFile.open(fileName, std::ios::in);
-		if (myFile.is_open()) {
-			std::cout << GREEN << "FILE OPEN" << RESET << std::endl;
+		std::cout << RED << "[FILE].replace cant be created" << RESET <<std::endl;
+		return 1;
+	}
+	replaceFileName.append(".replace", foundLast, 8);//Cree une string av[1]+".replace"
+	std::ofstream myReplaceFile; // Cree fichier "[FileName].replace"
+	myFile.open(fileName, std::ios::in);// File que je lis
+	if (myFile.is_open() && myFile.peek() != std::ifstream::traits_type::eof())// check si file existe et n'est pas vide
+	{
+		myReplaceFile.open(replaceFileName, std::ios::out);// ouvre "[FileName].replace"
+		if (myReplaceFile.is_open()) 
+		{
 			std::string s1(av[2]), s2(av[3]), line; // Creating variable i need
 			while (std::getline(myFile, line)) 
 			{
 				std::size_t foundStr = line.find(s1);
-				while (foundStr != std::string::npos) 
+				while (foundStr != std::string::npos)// check la meme ligne pour replacer toute les s1
 				{
 					line.replace(foundStr,s2.length(), s2);
-					std::cout << "line = " << line << std::endl;
 					foundStr = foundStr + s2.length();
 					foundStr = line.find(s1);
 				}
-				//ICI ecrire line dans un autre file
+				myReplaceFile << line;
+				myReplaceFile << "\n";
 			}
-			myFile.close();
+			myReplaceFile.close();
 		}
+		else
+		{
+			std::cout << RED << "file.replace dont opend retry pls" << RESET << std::endl;
+			myFile.close();
+			return 1;
+		}
+		myFile.close();
 	}
-	else {
-		std::cout << RED << "Your second argument need to be \"file.txt\"" << RESET << std::endl; 
+	else
+	{
+		std::cout << RED << "Your second argument need to be a existing file with string inside" << RESET << std::endl; 
+		myFile.close();
 		return 1;
 	}
-    
 }
