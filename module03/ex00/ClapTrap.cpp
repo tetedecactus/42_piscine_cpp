b4ed571d133c45cbb4bd8cde6a816e2975644f80
Oli_WSL <olabrecq@laptop_oli.localdomain>
2022-06-23 09:21:30 -0400

    m03 ex00 started

---
+++ module03/ex00/ClapTrap.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olabrecq <olabrecq@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 17:37:49 by olabrecq          #+#    #+#             */
+/*   Updated: 2022/06/22 17:37:49 by olabrecq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( void ) {}
+
+ClapTrap::ClapTrap( std::string name ) : _name(name), hitPoint(10), energyPoint(10), attDamage(0)
+{
+    std::cout << GREEN << _name << ": Constructor called " << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap( ClapTrap const & src, std::string name ) : _name(name), hitPoint(10), energyPoint(10), attDamage(0)
+{
+    *this = src;
+    std::cout << YELLOW << this->_name << ": Copy constructor called" << RESET << std::endl;
+    return ;
+}
+ClapTrap::~ClapTrap()
+{
+    std::cout << RED << _name ": Destructor called " << RESET << std::endl;
+}
+
+// ClapTrap & ClapTrap::operator=( ClapTrap const & rhs ) {
+
+// }
+
+void ClapTrap::beRepaired( unsigned int amount ) {
+    std::cout << GREEN << this._name << ": Have been repair of : " << amount << " point of life" << RESET << std::endl;
+}
+
+void ClapTrap::attack( const std::string& target ) {
+    std::cout << BLUE << this._name << ": Attack :" << target << "causing :" << this->hitPoint << " points of damage" << RESET << std::endl;
+}
+
+void ClapTrap::takeDamage( unsigned int amount ) {
+    std::cout  << YELLOW << this._name << ": Have take : " << amount << " of damage" << RESET << std::endl;
+}
---
+++ module03/ex00/ClapTrap.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olabrecq <olabrecq@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 17:38:13 by olabrecq          #+#    #+#             */
+/*   Updated: 2022/06/22 17:38:13 by olabrecq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+#include <iostream>
+#include <unistd.h>
+#include <string>
+
+# define BLUE "\033[0;36m"
+# define PINK "\033[0;35m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define RED "\033[0;31m"
+# define RESET "\033[0m"
+
+class ClapTrap
+{
+private:
+    std::string _name;
+    int hitPoint;
+    int energyPoint;
+    int attDamage;
+
+public:
+    ClapTrap( void );
+    ClapTrap( std::string name );
+    ClapTrap( ClapTrap const & src, std::string name );
+    ~ClapTrap();
+
+    // ClapTrap & operator=( ClapTrap const & rhs ); // Pas certain???
+
+    void attack( const std::string& target );
+    void takeDamage( unsigned int amount );
+    void beRepaired( unsigned int amount );
+};
+
+
+
+#endif
