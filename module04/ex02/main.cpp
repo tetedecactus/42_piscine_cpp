b0fb3ad5cb7f87968260de4963502a7feca1276f
Olivier Labrecque-lacasse <olabrecq@c2r4p1.42quebec.com>
2022-07-08 14:59:48 -0400

    m04 ex03

--- module04/ex02/main.cpp
+++ module04/ex02/main.cpp
@@ -22,6 +22,51 @@
 
 int main()
 {
-	
+	std::cout << GREEN << "Le but de cet Ex etait d'implementer la classe Animal de facon Abstrait pour que l'ont puisse pas l'utiliser ses methode, mais les utiliser ses child class" << RESET << std::endl;
+	std::cout << GREEN << "Pour tester, je vais creer 6 Animal" << std::endl;
+	std::cout << "1ier & 2ieme animal vont etre de type Dog & type Animal* Dog" << std::endl;
+	std::cout << "3ieme & 4ieme animal vont etre de type Cat & de type Animal* Cat" << std::endl;
+	std::cout << "5ieme & 6ieme animal vont etre de type Animal & Animal* Animal( will fail )" << RESET << std::endl;
+
+	std::cout << RED << "TEST : 1 -> TYPE DOG" << RESET << std::endl;
+	{
+	Animal *sanji =  new Dog();
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	Dog doggo;
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+
+	doggo.makeSound();
+	sanji->makeSound();
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	delete sanji;
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	}
+
+	std::cout << RED << "TEST : 1 -> TYPE DOG" << RESET << std::endl;
+	{
+	Animal *shanks =  new Cat();
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	Cat colonel;
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+
+	colonel.makeSound();
+	shanks->makeSound();
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	delete shanks;
+	std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	}
+
+	// std::cout << RED << "TEST : 3 -> TYPE ANIMAL" << RESET << std::endl;
+	// {
+	// Animal *animal =  new Dog();
+	// std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	// Animal ani;
+	// std::cout << BLUE << "===============================================================" << RESET << std::endl;
+
+	// ani.makeSound();
+	// animal->makeSound();
+	// delete animal;
+	// std::cout << BLUE << "===============================================================" << RESET << std::endl;
+	// }
     return 0;
 }
