02df864805431dc7c7aa746a165ac7fa5c42757a
win10 <olabrecq@DESKTOP-HLJKNLN.localdomain>
2022-07-14 22:42:43 -0400

    m04 ex03 started

---
+++ module04/ex03/ICharacter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 17:15:16 by marvin            #+#    #+#             */
+/*   Updated: 2022/07/14 17:15:16 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once 
+
+class ICharacter
+{
+    public:
+        virtual ~ICharacter() {}
+        virtual std::string const & getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
+};
