5ef3eb73fde02148f00737762d0d85b1798804f4
olabrecq_home_tour <olabrecq@DESKTOP-HLJKNLN.localdomain>
2022-08-02 22:14:14 -0400

    fuck module 06

--- module06/ex00/StaticCast.hpp
+++ module06/ex00/StaticCast.hpp
@@ -1,25 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Data.hpp                                           :+:      :+:    :+:   */
+/*   Cast.hpp                                  		    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/27 16:06:41 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/08/02 21:18:44 by marvin           ###   ########.fr       */
+/*   Created: 2022/08/02 21:39:08 by marvin            #+#    #+#             */
+/*   Updated: 2022/08/02 22:13:06 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #pragma once
 
-class Data
+class Cast
 {
-private:
-	/* data */
-public:
-	Data(/* args */);
-	~Data();
+	private:
+		char 	_c;
+		bool	_isC;
+
+		int 	_i;
+		bool 	_isI;
+
+		double 	_d;
+		bool 	_isD;
+		
+		float 	_f;
+		bool 	_isF;
+	public:
+		Cast(/* args */);
+		~Cast();
 };
+
+
+
+
+// : char, int, float ou double.
+// https://www.geeksforgeeks.org/static_cast-in-c-type-casting-operators/
+// https://en.cppreference.com/w/cpp/language/static_cast
+// https://cplusplus.com/reference/memory/static_pointer_cast/
 
 // https://en.cppreference.com/w/cpp/numeric/math/isnan
 // https://en.cppreference.com/w/cpp/numeric/math/isinf
