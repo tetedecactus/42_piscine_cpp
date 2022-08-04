862374728b2673134a0e91cdbef6f4239cc8bf75
olabrecq_home_tour <olabrecq@DESKTOP-HLJKNLN.localdomain>
2022-08-04 10:06:20 -0400

    +++

--- module06/ex02/Base.cpp
+++ module06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 21:37:41 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/08/03 22:11:50 by marvin           ###   ########.fr       */
+/*   Updated: 2022/08/04 09:58:47 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@ Base::~Base() {}
 
 Base* generate( void ) {
     
+    int i = (std::rand() % 3);
+    
+    switch ( i )
+    {
+    case 0:
+        return (new A());
+    case 1:
+        return (new B());
+    default:
+        return (new C());
+    }
 }
 
 void identify( Base* p ) {
--- module06/ex02/Base.hpp
+++ module06/ex02/Base.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Base.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 21:32:56 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/08/03 21:37:25 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/08/04 10:06:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,15 @@
 # define RESET "\033[0m"
 
 #include <iostream>
+#include <cstdlib>
+
 
 class Base
 {
 	public:
 		virtual ~Base();
 };
+
+Base* generate( void );
+void identify( Base* p);
+void identify( Base& p);
