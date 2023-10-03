f44361a7256a2ab78f8a93102c6b88aadc2b3fab
tetedecactus <o.labrecque.lacasse@gmail.com>
2023-10-03 17:33:30 -0400

    ++

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 01:13:50 by olabrecq          #+#    #+#             */
-/*   Updated: 2023/10/03 17:28:22 by olabrecq         ###   ########.fr       */
+/*   Updated: 2023/10/03 17:33:10 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,5 @@ class   BitcoinExchange  {
 
 
 // https://www.w3schools.in/cplusplus/working-with-files
+// https://quickref.me/cpp.html
+// https://hackingcpp.com/cpp/cheat_sheets.html
