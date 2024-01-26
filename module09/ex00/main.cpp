3c12fec1dcd0b9951b1e27378b54451c18fd3024
TeteDeCactus <o.labrecque.lacasse@gmail.com>
2024-01-26 16:28:04 -0500

    :

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -6,79 +6,93 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:19:52 by olabrecq          #+#    #+#             */
-/*   Updated: 2024/01/03 21:13:43 by olabrecq         ###   ########.fr       */
+/*   Updated: 2024/01/26 16:27:46 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 int main(int argc, const char **argv) {
-	// Constructors 1
-	try 
+	
 	{
-		BitcoinExchange btc_data;
-
-		if (btc_data.isValidArgs(argc) == false)
-			throw std::invalid_argument("Invalid number of arguments");
-		else 
-			btc_data.parseDB("data.csv");
-		btc_data.parseInputFile(argv[1]);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
+		std::cout << "Constructors with no Args" << std::endl;
+		// Constructors 1
+		try 
+		{
+			BitcoinExchange btc_data;
+	
+			if (btc_data.isValidArgs(argc) == false)
+				throw std::invalid_argument("Invalid number of arguments");
+			else 
+				btc_data.parseDB("data.csv");
+			btc_data.parseInputFile(argv[1]);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 	
 	std::cout << "-----------------------------------------" << std::endl;
 
-	//Constructors 2
-	try 
 	{
-		BitcoinExchange btc_data(argc);
-		btc_data.parseInputFile(argv[1]);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
+		std::cout << "Constructors with Args" << std::endl;
+		//Constructors 2
+		try 
+		{
+			BitcoinExchange btc_data(argc);
+			btc_data.parseInputFile(argv[1]);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 	
 	std::cout << "-----------------------------------------" << std::endl;
 
-	// Constructors 3
-	try
 	{
-		BitcoinExchange src;
-		BitcoinExchange btc_data(src);
+		// Constructors 3
+		std::cout << "Copy Constructor" << std::endl;
+		try
+		{
+			BitcoinExchange src;
+			BitcoinExchange btc_data(src);
+			
+			if (btc_data.isValidArgs(argc) == false)
+				throw std::invalid_argument("Invalid number of arguments");
+			else 
+				btc_data.parseDB("data.csv");
+			btc_data.parseInputFile(argv[1]);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	
+	std::cout << "-----------------------------------------" << std::endl;
+	
+	{
 		
-		if (btc_data.isValidArgs(argc) == false)
-			throw std::invalid_argument("Invalid number of arguments");
-		else 
-			btc_data.parseDB("data.csv");
-		btc_data.parseInputFile(argv[1]);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
-	std::cout << "-----------------------------------------" << std::endl;
-	
-	// Operateur d'affectation
-	try
-	{
-		BitcoinExchange src;
-		BitcoinExchange btc_data;
-		
-		if (btc_data.isValidArgs(argc) == false)
-			throw std::invalid_argument("Invalid number of arguments");
-		else 
-			btc_data.parseDB("data.csv");
-		btc_data.parseInputFile(argv[1]);
-		btc_data = src;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
+		// Operateur d'affectation
+		std::cout << "Assignation Operator" << std::endl;
+		try
+		{
+			BitcoinExchange src;
+			BitcoinExchange btc_data;
+			
+			if (btc_data.isValidArgs(argc) == false)
+				throw std::invalid_argument("Invalid number of arguments");
+			else 
+				btc_data.parseDB("data.csv");
+			btc_data.parseInputFile(argv[1]);
+			btc_data = src;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 
 	return (0);	
--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 10:39:12 by olabrecq          #+#    #+#             */
-/*   Updated: 2024/01/03 21:17:18 by olabrecq         ###   ########.fr       */
+/*   Updated: 2024/01/26 16:27:24 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,83 @@
 
 int main(int argc, char const *argv[]) {
 
-    // Constructors 1
-    try 
     {
-        RPN equation;
-        equation.setInput(argv[1]);
-        if (equation.checkInput(argv[1]) == false)
-            throw std::runtime_error("Error: Invalid input");
-        int result = equation.calculateRPN(argv[1]);
-        std::cout << "Result: " << result << std::endl;
+        std::cout << "Constructors with no Args" << std::endl;
+        // Constructors 1
+            try 
+        {
+            RPN equation;
+            equation.setInput(argv[1]);
+            if (equation.checkInput(argv[1]) == false)
+                throw std::runtime_error("Error: Invalid input");
+            int result = equation.calculateRPN(argv[1]);
+            std::cout << "Result: " << result << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
     std::cout << "-----------------------------------------" << std::endl;
     
-    // Constructors 2
-    try
     {
-        RPN equation(argc, argv);
-        int result = equation.calculateRPN(argv[1]);
-        std::cout << "Result: " << result << std::endl;
-        
-    }
-    catch(const std::exception& e)
+        std::cout << "Constructors with Args" << std::endl;
+        // Constructors 2
+        try
+        {
+            RPN equation(argc, argv);
+            int result = equation.calculateRPN(argv[1]);
+            std::cout << "Result: " << result << std::endl;
+            
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    
+    }   
+    std::cout << "-----------------------------------------" << std::endl;
+
     {
-        std::cerr << e.what() << '\n';
+        std::cout << "Copy Constructor" << std::endl;
+        // Constructors 3
+        try
+        {
+            RPN src;
+            RPN equation(src);
+            equation.setInput(argv[1]);
+            if (equation.checkInput(argv[1]) == false)
+                throw std::runtime_error("Error: Invalid input");
+            int result = equation.calculateRPN(argv[1]);
+            std::cout << "Result: " << result << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
     }
     
     std::cout << "-----------------------------------------" << std::endl;
 
-    // Constructors 3
-    try
     {
-        RPN src;
-        RPN equation(src);
-        equation.setInput(argv[1]);
-        if (equation.checkInput(argv[1]) == false)
-            throw std::runtime_error("Error: Invalid input");
-        int result = equation.calculateRPN(argv[1]);
-        std::cout << "Result: " << result << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
-    std::cout << "-----------------------------------------" << std::endl;
-
-    // Operators equals
-    try
-    {
-        RPN equation;
-        equation.setInput(argv[1]);
-        if (equation.checkInput(argv[1]) == false)
-            throw std::runtime_error("Error: Invalid input");
-        RPN equation2;
-        equation2 = equation;
-        int result = equation2.calculateRPN(argv[1]);
-        std::cout << "Result: " << result << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+        std::cout << "Assignation Operator" << std::endl;
+        // Operators equals
+        try
+        {
+            RPN equation;
+            equation.setInput(argv[1]);
+            if (equation.checkInput(argv[1]) == false)
+                throw std::runtime_error("Error: Invalid input");
+            RPN equation2;
+            equation2 = equation;
+            int result = equation2.calculateRPN(argv[1]);
+            std::cout << "Result: " << result << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
     }
     return 0;
 }
